d858fb54a0df9bbd8037bd997404860e4c076de2
puzanov <puzanov@gmail.com>
2016-12-09 21:26:47 +0600

    + added more tests for resolving URL via SSL

--- lib/utils_test.go
+++ lib/utils_test.go
@@ -14,3 +14,11 @@ func TestResolveTwoRedirects(t *testing.T) {
 	res := Resolve("http://ow.ly/Pkxu306YmRs", "http://ow.ly/", 10, false, "Googlebot")
 	assert.Equal(t, "http://maddevs.io/", res)
 }
+
+func TestResolveSSL(t *testing.T) {
+	res := Resolve("https://bit.ly/2hcXx5Z", "http://bit.ly/", 10, false, "Googlebot")
+	assert.Equal(t, "http://maddevs.io/", res)
+
+	res = Resolve("https://bit.ly/ID7AM5", "http://bit.ly/", 10, false, "Googlebot")
+	assert.Equal(t, "https://www.youtube.com/", res)
+}
