b20cbb33afcc93a6788adb033bdeda19b9ab60dd
puzanov <puzanov@gmail.com>
2017-01-02 17:40:21 +0600

    + added gzip middleware to gin

--- api/main.go
+++ api/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"gopkg.in/gin-gonic/gin.v1"
 	"github.com/maddevsio/spiderwoman/lib"
+	"github.com/gin-contrib/gzip"
 )
 
 func GetAPIEngine(dbPath string) *gin.Engine {
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
+	r.Use(gzip.Gzip(gzip.BestCompression))
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
