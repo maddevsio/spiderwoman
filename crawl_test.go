84085060681cf7624320df7f67657ce00e9238a2
puzanov <puzanov@gmail.com>
2017-03-19 12:32:10 +0600

    added todos for httpmock projects to check

--- crawl_test.go
+++ crawl_test.go
@@ -2,11 +2,18 @@ package main
 
 import "testing"
 
-func initMockHTTPServer() {}
+func initMockHTTPServer() {
+	// TODO: need to try
+	// https://github.com/h2non/gock
+	// http://www.mock-server.com/
+	// https://github.com/jarcoal/httpmock
+	// https://github.com/golang/mock
+}
 
 func TestCrawl(t *testing.T) {
 	initialize()
 	crawl()
 	// TODO: pass struct to initialize and crawl
 	// TODO: remove config dependencies
+
 }
