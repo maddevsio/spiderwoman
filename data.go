1367919635bf6a10c3d2332fa5e4cb41ed2ada0b
puzanov <puzanov@gmail.com>
2017-04-13 11:29:44 +0600

    changed excel generation

--- data.go
+++ data.go
@@ -4,23 +4,11 @@ import (
 	"strings"
 	"log"
 	"github.com/maddevsio/spiderwoman/lib"
+	"fmt"
 )
 
 func createXLS_BackupDB_Zip() {
-	days, _ := lib.GetAllDaysFromMonitor(sqliteDBPath)
-	log.Printf("Appendig XLS file with sheet %v", days[0])
-	err = lib.AppendExcelFromDB(sqliteDBPath, excelFilePath, days[0])
-	if (err != nil && strings.Contains(err.Error(), "no such file or directory")) {
-		lib.CreateEmptyExcel(excelFilePath)
-		log.Print("Trying to create all sheets in excel file")
-		for _, day := range days {
-			log.Printf("Appendig XLS file with sheet %v", day)
-			err = lib.AppendExcelFromDB(sqliteDBPath, excelFilePath, day)
-			if err != nil {
-				log.Print(err)
-			}
-		}
-	}
+	createAllXLSByDays()
 
 	log.Print("Backuping database")
 	err = lib.BackupDatabase(sqliteDBPath)
@@ -36,5 +24,13 @@ func createXLS_BackupDB_Zip() {
 		log.Printf("Zip error: %v", err)
 	} else {
 		log.Printf("Zipped xls file was saved in %v", excelZipFilePath)
+	}
+}
+
+func createAllXLSByDays() {
+	days, _ := lib.GetAllDaysFromMonitor(sqliteDBPath)
+	for _, day := range days {
+		log.Printf("Creating XLS file for day %v", day)
+		lib.CreateExcelFromDB(sqliteDBPath, fmt.Sprintf(excelFilePath, day), day)
 	}
 }
