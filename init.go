befa3dfe669531dc2bfa520d94ba4f34987a5e9e
puzanov <puzanov@gmail.com>
2017-06-15 18:32:51 +0600

    do not need to populate types on start

--- init.go
+++ init.go
@@ -8,8 +8,8 @@ import (
 func initialize(path Path) {
 	lib.CreateDBIfNotExists(path.SqliteDBPath)
 	lib.ClearResolveCache()
-	err = lib.PopulateHostsAndTypes(path.SqliteDBPath, path.TypesFilePath, path.TypesDefaultFilePath)
-	if err != nil {
-		log.Fatal("Types population error")
-	}
+	//err = lib.PopulateHostsAndTypes(path.SqliteDBPath, path.TypesFilePath, path.TypesDefaultFilePath)
+	//if err != nil {
+	//	log.Fatal("Types population error")
+	//}
 }
