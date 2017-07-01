fdf6680972e10a3abdbdb79142e05a6cd09f5586
puzanov <puzanov@gmail.com>
2017-07-01 21:28:21 +0600

    xls file new name for all data

--- api/main.go
+++ api/main.go
@@ -116,7 +116,7 @@ func GetAPIEngine(config simple_config.SimpleConfig) *gin.Engine {
 
 	// return xls on the fly for all data by the day
 	r.GET("/get-new-xls", func(c *gin.Context) {
-		xlsFileName := "new-" + c.Query("date") + ".xls"
+		xlsFileName := "all-" + c.Query("date") + ".xls"
 		xlsFilePath := "/tmp/" + xlsFileName
 		//lib.CreateExcelFromDB_NEW(config.GetString("db-path"), xlsFilePath, c.Query("date"))
 		c.Header("Content-Description", "File Transfer")
