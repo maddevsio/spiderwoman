3e712c5446f8666bd920e39dae0567effed079c5
puzanov <puzanov@gmail.com>
2017-07-23 16:29:24 +0600

    added rsync feature for deployment

---
+++ api/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rsync -v -P -e ssh api "$1":spiderwoman/api/api
+rsync -v -P -a -r -e ssh templates/ "$1":spiderwoman/api/templates
+rsync -v -P -a -r -e ssh images/ "$1":spiderwoman/api/images
+rsync -v -P -a -r -e ssh assets/ "$1":spiderwoman/api/assets
---
+++ deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+rsync -v -P -e ssh crawler "$1":spiderwoman/crawler
