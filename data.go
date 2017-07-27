0d461ee9c1d6b8f1ed88e07cba11b81a6521c40b
puzanov <puzanov@gmail.com>
2017-07-27 15:32:54 +0600

    sqlite backup removed

--- data.go
+++ data.go
@@ -6,20 +6,7 @@ import (
 	"fmt"
 )
 
-func createXLS_BackupDB(sqliteDBPath string) {
-	createAllXLSByDays(sqliteDBPath)
-
-	//log.Print("Backuping database")
-	//err = lib.BackupDatabase(sqliteDBPath)
-	//if (err != nil) {
-	//	log.Printf("Backup error: %v", err)
-	//} else {
-	//	log.Print("Database has been copied to /tmp/res.db")
-	//}
-}
-
 func createAllXLSByDays(sqliteDBPath string) {
-	
 	days, _ := lib.GetAllDaysFromMonitor(sqliteDBPath)
 	for _, day := range days {
 		log.Printf("Creating XLS file for day %v", day)
