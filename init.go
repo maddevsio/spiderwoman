c37aae5748ce100175fc0a0e926f6a741b00124b
puzanov <puzanov@gmail.com>
2017-07-27 15:36:02 +0600

    hosts population code removed

--- init.go
+++ init.go
@@ -7,8 +7,4 @@ import (
 func initialize(path Path) {
 	lib.CreateDBIfNotExists(path.SqliteDBPath)
 	lib.ClearResolveCache()
-	//err = lib.PopulateHostsAndTypes(path.SqliteDBPath, path.TypesFilePath, path.TypesDefaultFilePath)
-	//if err != nil {
-	//	log.Fatal("Types population error")
-	//}
 }
