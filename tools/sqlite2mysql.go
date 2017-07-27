beb925b436e820071a0af7f2c94a6d3e18bcec94
puzanov <puzanov@gmail.com>
2017-07-27 17:01:19 +0600

    mons are done

--- tools/sqlite2mysql.go
+++ tools/sqlite2mysql.go
@@ -6,6 +6,7 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"fmt"
+	"strings"
 )
 
 func main() {
@@ -25,27 +26,59 @@ func main() {
 	}
 	defer dbMysql.Close()
 
-	///////
+	/////// types
 
-	query := fmt.Sprint("SELECT hostname, hosttype FROM types")
+	//query := fmt.Sprint("SELECT hostname, hosttype FROM types")
+	//rows, err := dbSqlite.Query(query)
+	//if err != nil {
+	//	log.Panicf("Error getting data from types: %v", err)
+	//}
+	//defer rows.Close()
+	//
+	//for rows.Next() {
+	//	var hostname string
+	//	var hosttype string
+	//	err = rows.Scan(&hostname, &hosttype)
+	//	log.Printf("type: %s, host: %s", hosttype, hostname)
+	//	stmt, err := dbMysql.Prepare("insert into types(hostname, hosttype) values(?, ?)")
+	//	if err != nil {
+	//		log.Print(err)
+	//	}
+	//	_, err = stmt.Exec(hostname, hosttype)
+	//	if err != nil {
+	//		log.Print(err)
+	//	}
+	//}
+
+	/////// monitors
+
+	query := fmt.Sprint("SELECT id, source_host, external_link, count, external_host, created FROM monitor ORDER BY id DESC")
 	rows, err := dbSqlite.Query(query)
 	if err != nil {
-		log.Panicf("Error getting data from types: %v", err)
+		log.Panicf("Error getting data from mons: %v", err)
 	}
 	defer rows.Close()
 
 	for rows.Next() {
-		var hostname string
-		var hosttype string
-		err = rows.Scan(&hostname, &hosttype)
-		log.Printf("type: %s, host: %s", hosttype, hostname)
-		stmt, err := dbMysql.Prepare("insert into types(hostname, hosttype) values(?, ?)")
+		var id int
+		var source_host string
+		var external_link string
+		var count int
+		var external_host string
+		var created string
+		err = rows.Scan(&id, &source_host, &external_link, &count, &external_host, &created)
+		log.Printf("id %d", id)
+		stmt, err := dbMysql.Prepare("insert into monitor(source_host, external_link, count, external_host, created) values(?, ?, ?, ?, ?)")
 		if err != nil {
 			log.Print(err)
 		}
-		_, err = stmt.Exec(hostname, hosttype)
+
+		created = strings.Split(created, "T")[0]
+
+		_, err = stmt.Exec(source_host, external_link, count, external_host, created)
 		if err != nil {
 			log.Print(err)
 		}
+		stmt.Close()
 	}
 }
