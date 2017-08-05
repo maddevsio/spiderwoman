9a129e6983ace1ba1bfe2d65c6afbe9346875eb9
puzanov <puzanov@gmail.com>
2017-08-05 17:53:11 +0600

    jasonlvhit/gocron changed to carlescere/scheduler

--- actions.go
+++ actions.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"github.com/urfave/cli"
-	"github.com/jasonlvhit/gocron"
 	"log"
 	"github.com/maddevsio/spiderwoman/lib"
 	"os"
+	"github.com/carlescere/scheduler"
+	"runtime"
 )
 
 var path = Path{sqliteDBPath, lib.SourcesFilePath, lib.SourcesDefaultFilePath, lib.TypesFilePath, lib.TypesHDefaultFilePath}
@@ -17,19 +18,25 @@ func actionOnce(c *cli.Context) error {
 }
 
 func actionForever(c *cli.Context) error {
+	job := func() {
+		crawl(path)
+	}
 	initialize(path)
 	log.Print("All is OK. Starting cron job...")
 	if config.GetString("box") == "dev" {
 		log.Print("This is a dev box")
-		gocron.Every(1).Minute().Do(crawl) // this is for testing on dev box
+		scheduler.Every(1).Minutes().Run(job)
+		//gocron.Every(1).Minute().Do(crawl) // this is for testing on dev box
 	} else {
 		log.Print("This is production")
 		if config.GetString("start-time") == "" {
 			log.Fatal("You need to set start-time value in config.yaml")
 		}
-		gocron.Every(1).Day().At(config.GetString("start-time")).Do(crawl, path)
+		scheduler.Every().Day().At(config.GetString("start-time")).Run(job)
+		//gocron.Every(1).Day().At().Do(crawl, path)
 	}
-	<- gocron.Start()
+	//<- gocron.Start()
+	runtime.Goexit()
 	return nil
 }
 
